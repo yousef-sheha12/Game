7d70160e1812ab871e18ce1ae96e4c98dded7f8f
Yousef sheha <sheha8757@gmail.com>
2025-10-26 15:51:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+ import Games from './Games.jsx';
+ import SignPage from './SignPage.jsx';
+ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+ import './App.css';
+ 
+ function App() {
+   return (
+     <>
+      {/* <SignPage />
+       <Games /> */}
+
+     <Router>
+     <Routes>
+      <Route path='/play' element={<Games />} />
+      <Route path='/' element={<SignPage />} />
+     </Routes>
+     </Router>
+       
+      </>
+   )}
+ export default App;
---
+++ src/SignPage.jsx
@@ -0,0 +1,40 @@
+import './index.css';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+function SignPage() {
+  const navigate = useNavigate();
+  const [player1, setPlayer1] = useState('');
+  const [player2, setPlayer2] = useState('');
+
+  const handleSignin = () => {
+    navigate('/play', { state: { player1, player2 } });
+  };
+
+  return (
+    <>
+      <div className="sign">
+        <div className='container'>
+          <h1>Sign In</h1>
+          <label htmlFor="player1">PLAYER 1</label>
+          <input
+            type="text"
+            id="player1"
+            value={player1}
+            onChange={(e) => setPlayer1(e.target.value)}
+          />
+          <label htmlFor="player2">PLAYER 2</label>
+          <input
+            type="text"
+            id="player2"
+            value={player2}
+            onChange={(e) => setPlayer2(e.target.value)}
+          />
+          <button onClick={handleSignin}>SIGN IN</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignPage;
